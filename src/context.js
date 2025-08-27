a1f4482fc0c9dce59f73a936d9d0f07b1b20cdb0
LewdLeah <daisy.anais.197@gmail.com>
2025-08-27 15:29:20 -0400

    Update context.js

--- src/context.js
+++ src/context.js
@@ -1,6 +1,6 @@
 // Your "Context" tab should look like this
 const modifier = (text) => {
-  // Your other context modifier scripts go here (preferred)s
+  // Your other context modifier scripts go here (preferred)
   [text, stop] = AutoCards("context", text, stop);
   text = LocalizedLanguages("context", text);
   // Your other context modifier scripts go here (alternative)
