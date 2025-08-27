b24c61aa51f7a65e1b897921e34ad5ab8aff8457
LewdLeah <daisy.anais.197@gmail.com>
2025-08-27 15:26:11 -0400

    Delete src-without-auto-cards-included/context.js

--- src-without-auto-cards-included/context.js
+++
@@ -1,8 +0,0 @@
-// Your "Context" tab should look like this
-const modifier = (text) => {
-  // Your other context modifier scripts go here (preferred)
-  text = LocalizedLanguages("context", text);
-  // Your other context modifier scripts go here (alternative)
-  return { text };
-};
-modifier(text);
