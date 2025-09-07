4634bc195738a25075410f5f171e9b0353191687
LewdLeah <daisy.anais.197@gmail.com>
2025-09-07 15:02:07 -0400

    Update output.js

--- src (Without Auto-Cards)/output.js
+++ src (Without Auto-Cards)/output.js
@@ -1,6 +1,6 @@
 // Your "Output" tab should look like this
 const modifier = (text) => {
-  // Your other output modifier scripts go here
+  // Your output modifier scripts go here
   return { text };
 };
 modifier(text);
