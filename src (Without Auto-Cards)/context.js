807b21120d0f022bfa1d3b625cf41e1484278149
LewdLeah <daisy.anais.197@gmail.com>
2025-09-19 04:47:38 -0400

    Update context.js

--- src (Without Auto-Cards)/context.js
+++ src (Without Auto-Cards)/context.js
@@ -2,7 +2,7 @@
 const modifier = (text) => {
   // Your other context modifier scripts go here (preferred)
   text = LocalizedLanguages("context", text);
-  // Your other context modifier scripts go here (alternative)
+  // Your other context modifier scripts go here (risky)
   return { text };
 };
 modifier(text);
